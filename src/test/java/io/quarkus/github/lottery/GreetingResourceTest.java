ada8db9f745e421d839c771b175d47fa8104f5a8
Yoann Rodière <yoann@hibernate.org>
2022-09-06 15:39:24 +0200

    Add formatter plugin and fix formatting

--- src/test/java/io/quarkus/github/lottery/GreetingResourceTest.java
+++ src/test/java/io/quarkus/github/lottery/GreetingResourceTest.java
@@ -12,10 +12,10 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("Hello from RESTEasy Reactive"));
     }
 
 }
